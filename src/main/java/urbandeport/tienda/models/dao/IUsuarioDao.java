540b689dff639ce056fba6fd4b9368fc59031c7b
DegelMorningstar <fermaster911@gmail.com>
2023-04-05 20:36:25 -0600

    Revert "Se configura spring security con OAuth y JWT"
    This reverts commit d77b38bfa9ff3ca928116602a632d55a7b561b19.

--- src/main/java/urbandeport/tienda/models/dao/IUsuarioDao.java
+++
@@ -1,8 +0,0 @@
-package urbandeport.tienda.models.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import urbandeport.tienda.models.entity.Usuario;
-
-public interface IUsuarioDao extends JpaRepository<Usuario, Long> {
-    public Usuario findByEmail(String email);
-}
--- src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-/* Populate tabla clientes */
-INSERT INTO usuarios (nombre, apellidos, email,password,verificado, create_at,update_at) VALUES('Andrés', 'Guzmán', 'profesor@bolsadeideas.com','$2a$10$gXjv5HrDXbBH/RVcUPX1wegzeiNMwU0Kz48GKhFBvgOsQlTFH.zM.',true, '2018-01-01','2018-01-01');
-INSERT INTO usuarios (nombre, apellidos, email,password,verificado, create_at,update_at) VALUES('Yael', 'Montes', 'yael_montes@outlook.com','$2a$10$gXjv5HrDXbBH/RVcUPX1wegzeiNMwU0Kz48GKhFBvgOsQlTFH.zM.',true, '2018-01-01','2018-01-01');
-INSERT INTO roles (nombre) VALUES ('ROLE_USER');
-INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
-INSERT INTO usuarios_roles (usuario_id,roles_id) VALUES (1,1);
-INSERT INTO usuarios_roles (usuario_id,roles_id) VALUES (2,2);
